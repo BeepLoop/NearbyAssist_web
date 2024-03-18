387bc19cd1638e5e568ed751051e01e9a16ad721
Adrian Juntilla <adrianjuntilla38@gmail.com>
2024-03-18 15:58:13 +0800

    chore: format

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,8 @@
 export default function AboutPage() {
-  return (
-    <div>
-      <h1 className="my-2">This is from about page</h1>
-      <button className="bg-blue-500">Click Me</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="my-2">This is from about page</h1>
+			<button className="bg-blue-500">Click Me</button>
+		</div>
+	);
 }
