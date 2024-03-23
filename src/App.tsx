f69bc9e4382fb5210281d6785b60e7cf73816aed
Adrian Juntilla <adrianjuntilla38@gmail.com>
2024-03-23 22:29:07 +0800

    Modified LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import HomePage from "./pages/home_page";
 import LoginPage from "./pages/login_page";
 
 export default function App() {
-	return (
-		<ExampleContextProvider>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/example" element={<ExamplePage />} />
-			</Routes>
-		</ExampleContextProvider>
-	);
+    return (
+        <ExampleContextProvider>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/example" element={<ExamplePage />} />
+            </Routes>
+        </ExampleContextProvider>
+    );
 }
